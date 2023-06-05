d1e5e3e92a18aedff5a04419a45ef8806bd8463f
luozhiyong <luozhiyong@hlx.com>
2023-06-05 11:26:40 +0800

    ++

--- desktop-application/electron/main.js
+++ desktop-application/electron/main.js
@@ -4,8 +4,13 @@ const createWindow = () => {
     width: 1200,
     height: 800,
   });
+
   // 注意: 该路径为uniapp 发行H5/PC后生成的文件路径
-  win.loadFile("dist/build/h5/index.html");
+  // win.loadFile("dist/build/h5/index.html");
+
+  // 注意: 该路径为 npm run dev:h5 运行后开启的本地服务 根据实际情况修改
+  win.loadURL("http://localhost:5173"); // 有改动 新增
+
   // win.webContents.openDevTooLs() // 开启调试工具
 };
 app.whenReady().then(() => {
