1b525ea9829f0c1e0d925549e304f198c4a3b498
Luozhiyong <monsterluo1119@yeah.net>
2023-10-20 22:48:25 +0800

    ++

--- desktop-application/src/App.vue
+++ desktop-application/src/App.vue
@@ -1,36 +1,50 @@
 <script>
-import util from "./utils/util.js";
+import util from './utils/util.js';
 export default {
-  data() {
-    return {
-      u: util,
-    };
-  },
-  onLaunch: function () {
-    console.log("App Launch");
-  },
-  onShow: function () {
-    console.log("App Show");
-  },
-  onHide: function () {
-    console.log("App Hide");
-  },
+	data() {
+		return {
+			u: util,
+		};
+	},
+	onLaunch: function () {
+		console.log('App Launch');
+	},
+	onShow: function () {
+		console.log('App Show');
+	},
+	onHide: function () {
+		console.log('App Hide');
+	},
 };
 </script>
 
 <style lang="less">
 @font-face {
-  font-family: "Staatliches";
-  src: url("./src/static/font/Staatliches.ttf");
+	font-family: 'count';
+	src: url('./src/static/font/Staatliches.ttf');
+}
+@font-face {
+	font-family: 'en';
+	src: url('./src/static/font/en-word.woff2');
+}
+@font-face {
+	font-family: 'JetBrains';
+	src: url('./src/static/font/JetBrains-Mono.woff2');
 }
 .count {
-  font-family: "Staatliches";
+	font-family: 'count';
+}
+.en {
+	font-family: 'en';
+}
+.JetBrains {
+	font-family: 'JetBrains';
 }
 uni-page-body {
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 }
 .ivu-card-body {
-  padding: 0 !important;
+	padding: 0 !important;
 }
 </style>
